04fb50482c0c99039ba93ef46dae4b32b18d0673
Fardin Ahsan <fs.shafi253@gmail.com>
2020-06-25 00:37:54 +0600

    remove unused code

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -6,45 +6,22 @@ export default {
     ADD_TODO(state, todo) {
         state.todos = [...state.todos, todo];
     },
-    // GET_TODO(state, todo) {
-    //   state.newTodo = todo;
-    // },
-
-    // EDIT_TODO(state, todo) {
-    //   var todos = state.todos;
-    //   todos.splice(todos.indexOf(todo), 1);
-    //   state.todos = todos;
-    //   state.newTodo = todo.body;
-    // },
     REMOVE_TODO(state, index) {
         state.todos.splice(index, 1);
     },
     COMPLETE_TODO(state, index) {
         state.todos[index].completed = !state.todos[index].completed;
-    },
-    // CLEAR_TODO(state) {
-    //   state.newTodo = "";
-    // },
-
+    }
   },
   actions: {
-        getTodo({commit}, todo){
-        commit(`GET_TODO`, todo)
-      },
       addTodo({commit}, todo){
         commit(`ADD_TODO`, todo)
-      },
-      editTodo({commit}, todo){
-        commit(`EDIT_TODO`, todo)
       },
       removeTodo({commit}, index){
         commit(`REMOVE_TODO`, index)
       },
       completeTodo({commit}, index){
        commit(`COMPLETE_TODO`, index)
-      },
-      clearTodo({commit}){
-        commit('CLEAR_TODO')
       }
   },
 };
